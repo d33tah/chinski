f0050818a45b5ed2ce8626c878414e17c47b6381
Jacek Wielemborek <d33tah@gmail.com>
2018-05-12 23:33:05 +0200

    Add wiktionary_parse.

---
+++ wiktionary_parse/rust/src/main.rs
@@ -0,0 +1,35 @@
+extern crate xml;
+
+#[macro_use]
+extern crate serde_json;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use xml::reader::{EventReader, XmlEvent};
+
+fn main() {
+    let file = File::open("/dev/stdin").unwrap();
+    let file = BufReader::new(file);
+
+    let parser = EventReader::new(file);
+    let mut current_title: String = "".into();
+    let mut current_tag: String = "".into();
+    for e in parser {
+        match e {
+            Ok(XmlEvent::Characters(name)) => {
+                if current_tag == "title" {
+                    current_title = name.clone();
+                }
+                if current_tag == "text" && current_title.len() == 1 {
+                    let val = json!([current_title, name]);
+                    println!("{}", val.to_string());
+                }
+            }
+            Ok(XmlEvent::StartElement{ name, .. }) => {
+                current_tag = name.local_name;
+            }
+            _ => {}
+        }
+    }
+}
